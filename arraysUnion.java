0f9a321e0b3734e0433cf8814a1a56d20122f3b1
Vadym Nedoinov <vadim.nedoynov@gmail.com>
2022-01-15 14:02:15 +0200

    Arrays union program, first commit.

---
+++ arraysUnion.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class arraysUnion {
+    /**
+     * Program entry point.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+        int[] a1 = {6};
+        int[] a2 = {1,3,5};
+        int[] generalList = new int[a1.length + a2.length];
+        if (a1.length > 0 && a2.length > 0) {
+            int list_1_i = 0;
+            int list_2_i = 0;
+            outer:
+            for (int generalList_i = 0; generalList_i < generalList.length; generalList_i++) {
+                if (list_1_i < a1.length && a1[list_1_i] <= a2[list_2_i]) {
+                    generalList[generalList_i] = a1[list_1_i];
+                    list_1_i++;
+                } else {
+                    if (list_2_i < a2.length) {
+                        generalList[generalList_i] = a2[list_2_i];
+                        list_2_i++;
+                    }
+                    if (list_2_i == a2.length) {
+                        System.arraycopy(a1, list_1_i, generalList, generalList_i + 1, (generalList.length - generalList_i - 1));
+                        break outer;
+                    }
+                }
+            }
+            System.out.println(Arrays.toString(generalList));
+        } else {
+            System.out.println(a1.length > 0 ? Arrays.toString(a1) : Arrays.toString(a2));
+        }
+    }
+}
+// single line comment
+        /*
+        multi-line comment
+         */
